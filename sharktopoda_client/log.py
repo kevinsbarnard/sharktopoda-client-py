008d217a48b96d75c367d7ac7543f7993d80b08b
Kevin Barnard <kbarnard@mbari.org>
2023-06-15 14:42:45 -0700

    Remove default formatter/handler from loggers

--- sharktopoda_client/log.py
+++ sharktopoda_client/log.py
@@ -4,21 +4,6 @@ Logging utilities.
 
 import logging
 
-FORMATTER = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-STREAM_HANDLER = logging.StreamHandler()
-STREAM_HANDLER.setFormatter(FORMATTER)
-STREAM_HANDLER.setLevel(logging.DEBUG)
-
-
-def set_stream_level(level: int) -> None:
-    """
-    Set the log level of the stream handler.
-
-    Args:
-        level: The log level to set the stream handler to.
-    """
-    STREAM_HANDLER.setLevel(level)
-
 
 def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
     """
@@ -31,7 +16,6 @@ def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
         A logger with a given name.
     """
     logger = logging.Logger(name, level=level)
-    logger.addHandler(STREAM_HANDLER)
 
     return logger
 
